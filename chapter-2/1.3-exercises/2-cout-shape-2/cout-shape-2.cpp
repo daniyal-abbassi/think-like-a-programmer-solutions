df65893b1a1100029e34b0a1ee6c589d2e578aae
Dani <slimshady3753@protonmail.com>
2025-01-19 01:47:41 +0330

    Update cout-shape-2.cpp

--- chapter-2/1.3-exercises/2-cout-shape-2/cout-shape-2.cpp
+++ chapter-2/1.3-exercises/2-cout-shape-2/cout-shape-2.cpp
@@ -2,13 +2,32 @@
 using namespace std;
 
 // 8lines that goes:  2#, 4#, 6#, 8#, 8#, 6#, 4#, 2#
-
+// ##
+// ####
+// ######
+// ########
+// ########
+// ######
+// ####
+// ##
 int main()
 {
+    
     for (int i = -3; i <= 4; i++)
     {
-        for (int j = 1; j <= 8 - 2 * abs(i) ; j++)
+
+        if (i == 0)
         {
+            for (int j = 1; j <= 8 - 2 * abs(i); j++)
+            {
+
+                cout << "#";
+            }
+            cout<<endl;
+        }
+        for (int j = 1; j <= 8 - 2 * abs(i); j++)
+        {
+
             cout << "#";
         }
         cout << endl;
